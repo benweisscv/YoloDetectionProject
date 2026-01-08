d91496d487613fa71f96032a9857f80981be575e
Ben Weiss <ben.weiss.cv@gmail.com>
2026-01-08 14:35:12 +0200

    update model path

--- inference/model.py
+++ inference/model.py
@@ -3,7 +3,7 @@ import numpy as np
 import onnxruntime as ort
 from google.cloud import storage
 
-MODEL_PATH = "/app/models/yolo11.onnx"
+MODEL_PATH = "/models/yolo11.onnx"
 GCS_URI = "gs://bucket.benyosefweiss.com/yolo11.onnx"
 
 session = None
